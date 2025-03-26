dcd06449dd5d11c8b2b19d8d4fccf16d1cbe4b4b
simiadi <simonadam006@gmail.com>
2025-03-26 08:06:19 +0100

    services

---
+++ Solution.ValidationLibrary/ValidationRules/MaxDateRule.cs
@@ -0,0 +1,16 @@
+﻿namespace Solution.ValidationLibrary.ValidationRules;
+
+public class MaxDateRule<T>() : IValidationRule<T>
+{
+    public string ValidationMessage { get; set; } = $"Date can't be larger then {DateTime.Now}.";
+
+    public bool Check(object value)
+    {
+        if (!DateTime.TryParse(value?.ToString(), out DateTime data))
+        {
+            return false;
+        }
+
+        return data <= DateTime.Now;
+    }
+}
