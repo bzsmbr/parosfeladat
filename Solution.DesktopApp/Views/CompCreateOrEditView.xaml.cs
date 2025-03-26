402fa4a65b532f045a991b1edf2878cadb6978c7
simiadi <simonadam006@gmail.com>
2025-03-26 09:35:01 +0100

    views

---
+++ Solution.DesktopApp/Views/CompCreateOrEditView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Solution.DesktopApp.Views;
+
+public partial class CompCreateOrEditView : ContentPage
+{
+    public CompCreateOrEditViewModel ViewModel => this.BindingContext as CompCreateOrEditViewModel;
+
+    public static string Name => nameof(CompCreateOrEditView);
+
+    public CompCreateOrEditView(CompCreateOrEditViewModel viewModel)
+    {
+        this.BindingContext = viewModel;
+
+        InitializeComponent();
+    }
+}
