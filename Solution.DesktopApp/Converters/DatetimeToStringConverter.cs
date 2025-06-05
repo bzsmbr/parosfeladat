14eaf5352ca0b828220f75f9d4504e62047d31c9
simiadi <simonadam006@gmail.com>
2025-06-05 08:26:07 +0200

    remove time

---
+++ Solution.DesktopApp/Converters/DatetimeToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace Solution.DesktopApp.Converters
+{
+    public class DatetimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime datetimeValue)
+            {
+                string format = parameter as string ?? "d";
+                return datetimeValue.ToString(format, culture);
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str && DateTime.TryParse(str, culture, DateTimeStyles.None, out var result))
+            {
+                return result;
+            }
+            return Microsoft.Maui.Controls.Binding.DoNothing;
+        }
+    }
+}
